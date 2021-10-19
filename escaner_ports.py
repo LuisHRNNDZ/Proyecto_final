69d5671fd67760c109edc59daf5c0c96b1b4fe09
Romalimon03 <romario.limonhrnd@uanl.edu.mx>
2021-10-18 21:17:13 -0500

    Se agrego el codigo restante para generar un reporte

--- escaner_ports.py
+++ escaner_ports.py
@@ -4,37 +4,57 @@ import nmap
 
 def escan_ports():
 
-    ip = '127.0.0.1'
+    with open('reporte_scan.txt','w') as file:
 
-    scanner = nmap.PortScanner()
-    p_open = '-p '
-    result = scanner.scan(hosts=ip,arguments='-sT -n -Pn -T4')
-    count = 0
-    
-    print ("\nHost : %s" % ip)
-    print ("Estado : %s" % scanner[ip].state())
+        ip = ''
 
-    for proto in scanner[ip].all_protocols():
+        scanner = nmap.PortScanner()
+        p_open = '-p '
+        result = scanner.scan(hosts=ip,arguments='-sT -n -Pn -T4')
+        count = 0
         
-        print ("Protocolo : %s" % proto)
-        print ()
+        print ("\nHost : %s" % ip)
+        print ("Estado : %s" % scanner[ip].state())
 
-        lport = scanner[ip][proto].keys()
-        sorted(lport)
+        # Variables para reporte_san.txt
+        ho_st = "\nHost : %s" % ip
+        es_tado = "\nEstado : %s" % scanner[ip].state()
+        file.write(ho_st)
+        file.write(es_tado)
 
-        for port in lport:
+        for proto in scanner[ip].all_protocols():
+            
+            print ("Protocolo : %s" % proto)
+            print ()
 
-            print("port : %s\tstate : %s" % (port, scanner[ip][proto][port]["state"]))
+            # Variable para reporte_scan.txt
+            proto_colo = "\nProtocolo : %s" % proto
+            file.write(proto_colo)
 
-            if count == 0:
+            lport = scanner[ip][proto].keys()
+            sorted(lport)
 
-                p_open = p_open+str(port)
-                count = 1 
-            else:
-                p_open = p_open+","+str(port)
-    
-    print ("\nPuertos abiertos: "+ p_open +" "+str(ip))
+            for port in lport:
 
-escan_ports()
+                print("port : %s\tstate : %s" % (port, scanner[ip][proto][port]["state"]))
 
-#print(fuc_llama)
+                #Varible para repote_scan.txt
+                puer_tos_op = "\nport : %s\tstate : %s" % (port, scanner[ip][proto][port]["state"])
+                file.write(puer_tos_op)
+
+                if count == 0:
+
+                    p_open = p_open+str(port)
+                    count = 1 
+                else:
+                    p_open = p_open+","+str(port)
+        
+        print ("\nPuertos abiertos: "+ p_open +" "+str(ip))
+
+        # Variable para reporte_scan.txt
+        salida = "\nPuertos abiertos: "+ p_open +" "+str(ip)
+        file.write(salida)
+
+fuc_llama = escan_ports()
+
+print(fuc_llama)
