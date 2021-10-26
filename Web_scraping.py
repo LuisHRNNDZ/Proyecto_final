a181e6b3a3f204e737b95ce07c4c6a95943c3319
LuisHRNNDZ <luis.lopezh@uanl.edu.mx>
2021-10-26 16:03:32 -0500

    Añadí el api al web scraping
    Actualice el script de web_scraping para añadir el código correspondiente para el funcionamiento de la api. Falta documentación de las líneas de codigo.

--- Web_scraping.py
+++ Web_scraping.py
@@ -1,12 +1,20 @@
+import json
 import requests
 from bs4 import BeautifulSoup as bs
+from deep_translator import GoogleTranslator
 
-URL = "https://www.welivesecurity.com/la-es/"
 
 def noticias():
+    
     document = r"./noticias.txt"
     with open(document, 'w') as f:
 
+        translator = GoogleTranslator(source='auto', target="es")
+        apikey="apiKey=532e8814a0c243e9b2823f4a48af16eb"
+        url = ("https://newsapi.org/v2/everything?q=computing&sortBy=publishedAt&"
+               +apikey)
+        
+        URL = "https://www.welivesecurity.com/la-es/"
         page = requests.get(URL)
         soup = bs(page.content, "html.parser") 
         temas = soup.find_all('div', {"class": "text-wrapper col-sm-9 col-xs-8 no-padding"})
@@ -21,4 +29,19 @@ def noticias():
         
             if i == 2:
                 f.write(temas[i].text)
+                
+        r = requests.get(url)
+        data = json.loads(r.content)
+        articles = data['articles']
+        i = 0
+        f.write('____________________________________________________________________\n\n')
+        f.write('Noticia reciente del api NewsApi\n\n')
+        for d in articles:
+            if i == 1:
+                break
+            f.write("Título:"+ translator.translate(d['title'])+"\n")
+            f.write("Autor:"+ str(d['author'])+"\n")
+            f.write("Fecha de publicación:"+ d['publishedAt']+"\n")
+            f.write("Descripción:\n"+ translator.translate(d['description'])+"\n")
+            i = i + 1
 noticias()
