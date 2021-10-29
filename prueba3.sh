3d017f6c1ee6a493cdc8935f527da87819b635ab
LuisHRNNDZ <luis.lopezh@uanl.edu.mx>
2021-10-28 22:46:02 -0500

    Update prueba3.sh
    Se actualizaron comentaros en el script.

--- prueba3.sh
+++ prueba3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+#Instalar apt-get install enscript ghostscript
 
 
-
-enscript -M A4 --margins=1:1:1:1 -f Courier7 prueba1 -p prueba1.ps
+enscript -M A4 --margins=1:1:1:1 -f Courier7 noticias.txt reporte_scan.txt -p prueba1.ps
 
 ps2pdf prueba1.ps prueba3.pdf
