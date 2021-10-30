7047acb4a8d76540589cc650d4ded3ec15e316fa
Knak23 <91349470+Knak23@users.noreply.github.com>
2021-10-30 15:18:11 -0500

    Update PEP8 Descifrado.py

--- Descifrado.py
+++ Descifrado.py
@@ -2,20 +2,20 @@ def Desencriptar(Frase):
     FraseDes = ''
     for letra in Frase:
         encontrado = False
-        for x,y in abecedario.items():
+        for x, y in abecedario.items():
             if letra == y:
                 FraseDes += x
                 encontrado = True
-        if not encontrado: 
+        if not encontrado:
             FraseDes += letra
     return FraseDes
 abecedario = {
-    'A':'E', 'B':'F', 'C':'G', 'D':'H', 'E':'I',
-    'F':'J', 'G':'K', 'H':'L', 'I':'M', 'J':'N',
-    'K':'O', 'L':'P', 'M':'Q', 'N':'R', 'O':'S',
-    'P':'T', 'Q':'U', 'R':'V', 'S':'W', 'T':'X',
-    'U':'Y', 'V':'Z', 'W':'A', 'X':'B', 'Y':'C',
-    'Z':'D'
+    'A': 'E', 'B': 'F', 'C': 'G', 'D': 'H', 'E': 'I',
+    'F': 'J', 'G': 'K', 'H': 'L', 'I': 'M', 'J': 'N',
+    'K': 'O', 'L': 'P', 'M': 'Q', 'N': 'R', 'O': 'S',
+    'P': 'T', 'Q': 'U', 'R': 'V', 'S': 'W', 'T': 'X',
+    'U': 'Y', 'V': 'Z', 'W': 'A', 'X': 'B', 'Y': 'C',
+    'Z': 'D'
 }
 print("Menú\n1) Desencriptar\n2) Salir")
 x = int(input("Opción: "))
@@ -29,4 +29,3 @@ while x != 2:
         print("Error! Opción no válida")
     print("Menú\n1) Desencriptar\n2) Salir")
     x = int(input("Opción: "))
-    
