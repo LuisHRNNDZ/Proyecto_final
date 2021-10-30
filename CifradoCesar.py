414cec26077f6c184277165c8a0a69d143d37395
Knak23 <91349470+Knak23@users.noreply.github.com>
2021-10-30 15:15:39 -0500

    Update PEP8 Cifrado.py

--- CifradoCesar.py
+++ CifradoCesar.py
@@ -1,23 +1,23 @@
-#Cifrado Cesar doble
+# Cifrado Cesar doble
 def Encriptar(Frase):
-    FraseEnc = '' 
-    for letra in Frase: 
+    FraseEnc = ''
+    for letra in Frase:
         encontrado = False
-        for x,y in abc.items():
+        for x, y in abecedario.items():
             if letra == x:
-                FraseEnc += y 
+                FraseEnc += y
                 encontrado = True
-        if not encontrado: 
+        if not encontrado:
             FraseEnc += letra
     return FraseEnc
 
-abc = {
-    'A':'E', 'B':'F', 'C':'G', 'D':'H', 'E':'I',
-    'F':'J', 'G':'K', 'H':'L', 'I':'M', 'J':'N',
-    'K':'O', 'L':'P', 'M':'Q', 'N':'R', 'O':'S',
-    'P':'T', 'Q':'U', 'R':'V', 'S':'W', 'T':'X',
-    'U':'Y', 'V':'Z', 'W':'A', 'X':'B', 'Y':'C',
-    'Z':'D'
+abecedario = {
+    'A': 'E', 'B': 'F', 'C': 'G', 'D': 'H', 'E': 'I',
+    'F': 'J', 'G': 'K', 'H': 'L', 'I': 'M', 'J': 'N',
+    'K': 'O', 'L': 'P', 'M': 'Q', 'N': 'R', 'O': 'S',
+    'P': 'T', 'Q': 'U', 'R': 'V', 'S': 'W', 'T': 'X',
+    'U': 'Y', 'V': 'Z', 'W': 'A', 'X': 'B', 'Y': 'C',
+    'Z': 'D'
 }
 
 print("Menú\n1) Encriptar\n2) Salir")
@@ -27,11 +27,10 @@ while x != 2:
         frase = input('Frase en texto claro: ')
         frase = frase.upper()
         encriptadouno = Encriptar(frase)
-        FraseEnc= Encriptar(encriptadouno)
+        FraseEnc = Encriptar(encriptadouno)
         print(FraseEnc)
     else:
         print("Error! Opción no válida")
     print("Menú\n1) Encriptar\n2) Salir")
     x = int(input("Opción: "))
-
 
