cc86c52196000e5c90fb0ff6430055203a49996a
NoeRivera <carlos.riveray@uanl.edu.mx>
2021-11-07 19:35:00 -0600

    .

--- metadata_pdf.py
+++ metadata_pdf.py
@@ -12,17 +12,12 @@ def printMeta():
         with open(document, 'w', errors="ignore") as f:
             # Manda a llamar a el script de bash
             subprocess.call('./converter_pdf.sh')
-            # guarda la ruta actual en la variable ruta
             ruta = os.getcwd()
-            # choosedir / cambia al directorio actual en caso de que estes en otro
             os.chdir(ruta)
             # Un for para iterar cada archivo de la carpeta
             for root, dirs, files in os.walk(".", topdown=False):
-                # Un for para iterar los nombres de las carpetas
                 for name in files:
-                    # ext = nombre del archivo en minusculas
                     ext = name.lower() .rsplit('.', 1)[-1]
-                    # por cada archivo que termine en pdf
                     if ext in ['pdf']:
                         # imprime metadata
                         f.write("[+] Metadata for file: %s " %
@@ -35,10 +30,8 @@ def printMeta():
                             f.write('[+] ' + metaItem + ':' +
                                     docInfo[metaItem] + "\n")
                         f.write("\n")
-            # remueve prueba1.ps 
             os.remove('./prueba1.ps')
 
-    # Windows
     except OSError:
         print("Tu sistema es operativo es Windows, "
               "no se ejecutara el modulo METADATA")
